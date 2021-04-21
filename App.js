f76e76a7d039abe47bc83e9236de09abd6c4afe0
sasakikoka <torop115@gmail.com>
2021-04-21 13:38:37 +0900

    1st class

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
+import React from 'react';
 import { List } from "./List";
-import { useState } from 'react';
-
-function App() {
-  const [description, setDescription] = useState('クリック前の表示');
-  const changeDescription = () => {
-    setDescription('クリック後の表示です');
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { description: 'クリック前の表示です'}
   }
-  return(
-    <div>
-      { description }
-      <List title="取り扱い言語一覧"/>
-      <button onClick={changeDescription}>ボタン</button>
-    </div>
-  );
+  changeDescription() {
+    this.setState({
+      description: 'クリック後の表示です'
+    })
+  }
+  render() {
+    const { description } = this.state;
+    return (
+      <div>
+        { description }
+        <List title="取り扱い言語一覧" />
+        <button onClick={() => this.changeDescription()}>ボタン</button>
+      </div>
+    )
+  }
 }
 export default App;
